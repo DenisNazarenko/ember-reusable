22cb52fa807d1c7a7047eee16feb451f24f6cf06
Denis Nazarenko <den.nazarenko@icloud.com>
2018-04-20 18:10:38 +0300

    Form submit component

---
+++ app/components/form-submit.js
@@ -0,0 +1,10 @@
+import FormButton from 'webapp/components/form-button';
+
+/**
+ * Form submit button.
+ */
+export default FormButton.extend({
+  classNames: ['form-button--submit'],
+  layoutName: 'components/form-button',
+  type: 'submit'
+});
